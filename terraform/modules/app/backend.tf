87c67557f4be57be9d69e419ef70a2bb4d06e8ba
Samuel Nadim <wnadim2992@gmail.com>
2026-01-11 01:11:25 -0500

    fix name subnet

--- terraform/modules/app/backend.tf
+++ terraform/modules/app/backend.tf
@@ -5,7 +5,7 @@ module "emp_track_middletier_pe_snet" {
   source      = "../../modules/subnet"
   subnet_type = "private"
   cidr        = var.emp_track_middletier_pe_snet_cidr
-  subnet_name = "${var.project_name}-${var.environment}-${var.region}-btier-pe-snet"
+  subnet_name = "${var.project_name}-${var.environment}-${var.region}-mid-pe-snet"
   rg_name     = module.rg.rg_name
   vnet_name   = module.vnet.vnet_name
   region      = var.region  
@@ -16,7 +16,7 @@ module "emp_track_middletier_vnetintegration_snet" {
   source      = "../../modules/subnet"
   subnet_type = "private"
   cidr        = var.emp_track_middletier_vnetintegration_snet_cidr
-  subnet_name = "${var.project_name}-${var.environment}-${var.region}-front-vnetinteg-snet"
+  subnet_name = "${var.project_name}-${var.environment}-${var.region}-mid-vnetinteg-snet"
   rg_name     = module.rg.rg_name
   vnet_name   = module.vnet.vnet_name
   region      = var.region
