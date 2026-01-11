cb19ccc962321221af31d1bd316dac63545fea10
Samuel Nadim <wnadim2992@gmail.com>
2026-01-10 22:29:01 -0500

    change privacy of subnets

--- terraform/modules/app/frontend.tf
+++ terraform/modules/app/frontend.tf
@@ -1,31 +1,31 @@
-
+#subnets
 
 #front end public appgw subnet
 module "emp_track_frontend_appgw_snet" {
-  source      = "../../modules/subnet"
-  subnet_type = "public"
-  cidr        = var.emp_track_frontend_appgw_snet_cidr
-  subnet_name = "${var.project_name}-${var.environment}-appgw-pub-snet"
-  rg_name     = module.rg.rg_name
-  vnet_name   = module.vnet.vnet_name
+    source = "../../modules/subnet"
+    subnet_type = "public"
+    cidr = var.emp_track_frontend_appgw_snet_cidr
+    subnet_name = "${var.project_name}-${var.environment}-appgw-pub-snet"
+    rg_name = module.rg.rg_name
+    vnet_name = module.vnet.vnet_name
 }
 
 #front end private endpoint subnet
 module "emp_track_frontend_pe_snet" {
-  source      = "../../modules/subnet"
-  subnet_type = "public"
-  cidr        = var.emp_track_frontend_pe_snet_cidr
-  subnet_name = "${var.project_name}-${var.environment}-front-pe-snet"
-  rg_name     = module.rg.rg_name
-  vnet_name   = module.vnet.vnet_name
+    source = "../../modules/subnet"
+    subnet_type = "private"
+    cidr = var.emp_track_frontend_pe_snet_cidr
+    subnet_name = "${var.project_name}-${var.environment}-front-pe-snet"
+    rg_name = module.rg.rg_name
+    vnet_name = module.vnet.vnet_name
 }
 
 #front end vnet integration subnet
 module "emp_track_frontend_vnetintegration_snet" {
-  source      = "../../modules/subnet"
-  subnet_type = "private"
-  cidr        = var.emp_track_frontend_vnetintegration_snet_cidr
-  subnet_name = "${var.project_name}-${var.environment}-front-vnetinteg-snet"
-  rg_name     = module.rg.rg_name
-  vnet_name   = module.vnet.vnet_name
+    source = "../../modules/subnet"
+    subnet_type = "private"
+    cidr = var.emp_track_frontend_vnetintegration_snet_cidr
+    subnet_name = "${var.project_name}-${var.environment}-front-vnetinteg-snet"
+    rg_name = module.rg.rg_name
+    vnet_name = module.vnet.vnet_name
 }
