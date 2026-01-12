7a81fbb6851393aeeddfb96728cea40019622973
Samuel Nadim <wnadim2992@gmail.com>
2026-01-12 19:17:04 +0000

    lock down versions

--- terraform/src/providers.tf
+++ terraform/src/providers.tf
@@ -1,9 +1,10 @@
 # Configure the Microsoft Azure Provider
 terraform {
+  required_version = ">= 1.9.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.0"
+      version = "4.14.0"
     }
   }
 }
