39c12f27f6fa3ec22b50a2d28af2e41d592210ca
Samuel Nadim <wnadim2992@gmail.com>
2026-01-12 21:50:43 +0000

    add release variables

---
+++ terraform/src/variables_dynamic.tf
@@ -0,0 +1,9 @@
+variable "funcapp_name" {
+  description = "The name of the Function App passed from GitHub Actions"
+  type        = string
+}
+
+variable "webapp_name" {
+  description = "The name of the Web App passed from GitHub Actions"
+  type        = string
+}
