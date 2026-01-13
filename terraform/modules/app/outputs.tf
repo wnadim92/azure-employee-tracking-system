d112270c77a99b95346a01f57b49d4d8956186ee
Samuel Nadim <wnadim2992@gmail.com>
2026-01-13 01:29:34 +0000

    fix deployment type based off sku

--- terraform/modules/app/outputs.tf
+++ terraform/modules/app/outputs.tf
@@ -5,3 +5,23 @@ output "webapp_name" {
 output "funcapp_name" {
   value = module.emp_track_middle_funcapp.name
 }
+
+output "vnet_id" {
+  value = module.vnet.vnet_id
+}
+
+output "vnet_name" {
+  value = module.vnet.vnet_name
+}
+
+output "vnet_address_spaces" {
+  value = module.vnet.vnet_address_spaces
+}
+
+output "rg_id" {
+  value = module.rg.rg_id
+}
+
+output "rg_name" {
+  value = module.rg.rg_name
+}
