60a2de69cc9cb944f0334a7abeb4f3be52725591
Samuel Nadim <wnadim2992@gmail.com>
2026-01-13 00:35:35 +0000

    fix deployment type based off sku

--- terraform/modules/app/outputs.tf
+++ terraform/modules/app/outputs.tf
@@ -1,45 +1,7 @@
-output "vnet_id" {
-  value       = module.vnet.vnet_id
-  description = "The unique ID of the Virtual Network."
+output "webapp_name" {
+  value = module.emp_track_frontend_appsvc.name
 }
 
-output "vnet_name" {
-  value       = module.vnet.vnet_name
-  description = "The name of the Virtual Network."
-}
-
-output "vnet_address_spaces" {
-  value       = module.vnet.vnet_address_spaces
-  description = "The address spacse assigned to the Virtual Network."
-}
-
-output "rg_id" {
-  description = "The ID of the resource group."
-  value       = module.rg.rg_id
-}
-
-output "rg_name" {
-  description = "The name of the resource group."
-  value       = module.rg.rg_name
-}
-
-# Output for Cosmos DB Private DNS Zone
-output "cosmos_dns_zone_id" {
-  value       = azurerm_private_dns_zone.cosmos.id
-  description = "The ID of the Private DNS Zone for Cosmos DB NoSQL."
-}
-
-# Output for Web Apps/Function Apps Private DNS Zone
-output "web_dns_zone_id" {
-  value       = azurerm_private_dns_zone.web.id
-  description = "The ID of the Private DNS Zone for App Services and Function Apps."
-}
-
-# Optional: Output the Zone Names for reference in records
-output "cosmos_dns_zone_name" {
-  value = azurerm_private_dns_zone.cosmos.name
-}
-
-output "web_dns_zone_name" {
-  value = azurerm_private_dns_zone.web.name
+output "funcapp_name" {
+  value = module.emp_track_middle_funcapp.name
 }
