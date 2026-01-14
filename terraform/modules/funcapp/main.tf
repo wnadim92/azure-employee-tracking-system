51d83369673f64fde974406279efe03081eb5e0e
Samuel Nadim <wnadim2992@gmail.com>
2026-01-14 21:38:49 +0000

    Add

--- terraform/modules/funcapp/main.tf
+++ terraform/modules/funcapp/main.tf
@@ -49,8 +49,6 @@ resource "azurerm_linux_function_app" "this" {
   site_config {
     vnet_route_all_enabled            = true
     always_on                         = true
-    health_check_eviction_time_in_min = 2
-    health_check_path                 = "/health"
 
     cors {
       allowed_origins     = var.allowed_origins
