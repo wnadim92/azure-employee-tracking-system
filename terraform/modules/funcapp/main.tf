2b39e2db8cbe2ef7afe61961bf43478ad0140106
Samuel Nadim <wnadim2992@gmail.com>
2026-01-14 19:26:48 +0000

    fix

--- terraform/modules/funcapp/main.tf
+++ terraform/modules/funcapp/main.tf
@@ -12,8 +12,8 @@ resource "azurerm_linux_function_app" "this" {
   resource_group_name  = var.rg_name
   location             = var.region
   service_plan_id      = azurerm_service_plan.this.id
-  storage_account_name = module.storage.storage_account_name
-  storage_account_access_key = module.storage.primary_access_key
+  storage_account_name = azurerm_storage_account.this.name
+  storage_account_access_key = azurerm_storage_account.this.primary_access_key
 
   #storage_uses_managed_identity = false
   virtual_network_subnet_id     = var.vnet_integration_subnet_id
@@ -26,8 +26,8 @@ resource "azurerm_linux_function_app" "this" {
   zip_deploy_file = var.zip_package_path
 
   app_settings = {
-    "AzureWebJobsStorage"                                = module.storage.primary_connection_string
-    "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"           = module.storage.primary_connection_string
+    "AzureWebJobsStorage"                                = azurerm_storage_account.this.primary_connection_string
+    "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING"           = azurerm_storage_account.this.primary_connection_string
     "WEBSITE_CONTENTSHARE"                               = azurerm_storage_share.this.name
     "WEBSITE_DNS_SERVER"                                 = "168.63.129.16" # Required for Azure DNS so can reach azure DNS such as for private endpoint DNS Zones
     "WEBSITE_SKIP_CONTENT_SHARE_VALIDATION"              = "1"
@@ -42,7 +42,8 @@ resource "azurerm_linux_function_app" "this" {
     "COSMOS_DB_NAME"                                     = var.database_name
     "DB_DATABASE_NAME"                                   = var.database_name
     "WEBSITE_RUN_FROM_PACKAGE"                           = "1"
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
+    //"SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
+    // "DOCDBCONNSTR_" = 
   }
 
   site_config {
@@ -64,20 +65,34 @@ resource "azurerm_linux_function_app" "this" {
 
 resource "azurerm_storage_share" "this" {
   name                 = lower(var.funcapp_name)
-  storage_account_name = module.storage.storage_account_name
+  storage_account_name = azurerm_storage_account.this.name
+  resource_group_name  = var.rg_name
+  location             = var.region
   quota                = 50
 }
 
-module "storage" {
-  source                        = "../storage"
-  strg_name                     = lower(substr(replace(var.funcapp_name, "-", ""), 0, 24))
-  rg_name                       = var.rg_name
-  region                        = var.region
-  subnet_id                     = var.pe_subnet_id
-  principal_id                  = var.uami_principal_id
-  blob_dns_zone_id              = var.blob_dns_zone_id
-  file_dns_zone_id              = var.file_dns_zone_id
-  table_dns_zone_id             = var.table_dns_zone_id
-  queue_dns_zone_id             = var.queue_dns_zone_id
+resource "azurerm_storage_account" "this" {
+  name                     = lower(substr(replace(var.funcapp_name, "-", ""), 0, 24))
+  resource_group_name      = var.rg_name
+  location                 = var.region
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  # SECURITY: Deny all public access
   public_network_access_enabled = var.public_network_access_enabled
+  network_rules {
+    default_action = "Allow"
+    bypass         = ["AzureServices"]
+  }
+}
+
+
+resource "azurerm_role_assignment" "storage_access" {
+  scope                = azurerm_storage_account.this.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = var.principal_id
+
+  # Recommended for new identities to prevent replication lag errors
+  skip_service_principal_aad_check = true
 }
