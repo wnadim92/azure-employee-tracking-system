59f6d5fd691b4e42b93e71bfaeedf29de207c5a5
Samuel Nadim <wnadim2992@gmail.com>
2026-01-14 03:06:22 +0000

    add

--- appcode/frontend/my-app/src/App.js
+++ appcode/frontend/my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
   });
 
   // Access the environment variable injected by Docker
-  const API_BASE = process.env.REACT_APP_API_BASE_URL || 'http://localhost:7071/api';
+  const API_BASE = process.env.REACT_APP_API_BASE_URL || 'http://localhost:7071';
 
   useEffect(() => {
     fetchEmployees();
--- appcode/frontend/my-app/src/services/api.js
+++ appcode/frontend/my-app/src/services/api.js
@@ -26,7 +26,7 @@
 // };
 
 
-const API_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:7071/api";
+const API_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:7071";
 
 export const getEmployees = async () => {
   const response = await fetch(`${API_URL}/employees`);
