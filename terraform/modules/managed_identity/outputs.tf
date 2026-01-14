117e79a546e9e96357a613b53e501e611837b31e
Samuel Nadim <wnadim2992@gmail.com>
2026-01-14 07:19:27 +0000

    rename

--- terraform/modules/managed_identity/outputs.tf
+++ terraform/modules/managed_identity/outputs.tf
@@ -3,7 +3,6 @@ output "principal_id" {
   description = "The Principal ID of the User Assigned Managed Identity."
 }
 
-# 2. Use this for App Settings (Function App Configuration)
 output "client_id" {
   value       = azurerm_user_assigned_identity.this.client_id
   description = "The Client ID used in app_settings to select this identity."
