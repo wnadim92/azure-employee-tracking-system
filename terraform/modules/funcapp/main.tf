43152c94f018a89d298b731f9e8ee3663348f291
Samuel Nadim <wnadim2992@gmail.com>
2026-01-19 23:13:06 -0500

    verify zip content

--- terraform/modules/funcapp/main.tf
+++ terraform/modules/funcapp/main.tf
@@ -8,22 +8,18 @@ resource "azurerm_service_plan" "this" {
 }
 
 resource "azurerm_linux_function_app" "this" {
-  name                 = var.funcapp_name
-  resource_group_name  = var.rg_name
-  location             = var.region
-  service_plan_id      = azurerm_service_plan.this.id
-  storage_account_name = azurerm_storage_account.this.name
-  storage_account_access_key = azurerm_storage_account.this.primary_access_key
-
-  #storage_uses_managed_identity = false
+  name                          = var.funcapp_name
+  resource_group_name           = var.rg_name
+  location                      = var.region
+  service_plan_id               = azurerm_service_plan.this.id
+  storage_account_name          = azurerm_storage_account.this.name
+  storage_account_access_key    = azurerm_storage_account.this.primary_access_key
   virtual_network_subnet_id     = var.vnet_integration_subnet_id
 
   identity {
     type         = "UserAssigned"
     identity_ids = [var.uami_resource_id]
   }
-
-  # zip_deploy_file = var.zip_package_path
 
   app_settings = {
     "AzureWebJobsStorage"                                = azurerm_storage_account.this.primary_connection_string
