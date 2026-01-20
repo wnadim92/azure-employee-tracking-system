4ff00ec3ecc18b6ac3950c902a1cb931dfad6863
Samuel Nadim <wnadim2992@gmail.com>
2026-01-20 00:24:13 -0500

    verify zip content

--- terraform/modules/funcapp/main.tf
+++ terraform/modules/funcapp/main.tf
@@ -38,8 +38,8 @@ resource "azurerm_linux_function_app" "this" {
     "COSMOS_DB_NAME"                                     = var.database_name
     "DB_DATABASE_NAME"                                   = var.database_name
     "WEBSITE_RUN_FROM_PACKAGE"                           = "1"
-    "SCM_DO_BUILD_DURING_DEPLOYMENT"                     = "true"
-    "ENABLE_ORYX_BUILD"                                  = "true"
+    "SCM_DO_BUILD_DURING_DEPLOYMENT"                     = "false"
+    "ENABLE_ORYX_BUILD"                                  = "false"
     "AzureWebJobsFeatureFlags"                           = "EnableWorkerIndexing"
     "PYTHONPATH"                                         = "/home/site/wwwroot/.python_packages/lib/site-packages"
     "AzureWebJobsScriptRoot"                            = "/home/site/wwwroot"
